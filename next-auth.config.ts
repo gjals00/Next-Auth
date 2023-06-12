245eed21258e4f38ff5e6a30f3c42923813974b8
hm <gjals00@gmail.com>
2023-06-12 16:38:18 +0900

    Initial commit

--- next-auth.config.ts
+++ next-auth.config.ts
@@ -1,15 +1,18 @@
-import NextAuth, { User } from 'next-auth';
+import NextAuth, { Session, User } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import KakaoProvider from 'next-auth/providers/kakao';
 import NaverProvider from 'next-auth/providers/naver';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface CustomSession extends Session {
+  accessToken?: string; // Add an optional accessToken property
+}
+
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID!,
@@ -28,5 +31,22 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: async ({ session, user }: { session: CustomSession; user: User }) => {
 
+      // Log the access token if it exists
+      if (session.accessToken) {
+        console.log('Access token:', session.accessToken);
+      }
+
+      // Update the sessionToken in the database
+      await prisma.session.update({
+        where: { id: user.name || '' },
+        data: { sessionToken: session.accessToken },
+      });
+
+      return Promise.resolve(session);
+    },
+  },
+});
